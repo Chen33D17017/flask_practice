3a5e2ea770708728d235a4980623cfd79c770bb8
Chen33D17017 <s894117@gmail.com>
2019-11-05 14:44:26 +0900

    Complete Pagination

--- flask_main/forms.py
+++ flask_main/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField 
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_main.models import User
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +14,7 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', 
             validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+    # Must start with 'validate' ?
     def validate_username(serf, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
@@ -29,3 +32,27 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Submit')
 
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+            validators=[DataRequired(), Length(min=2, max=20)])  
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+    # Must start with 'validate' ?
+    def validate_username(serf, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another name')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That username is taken. Please choose another name')
+    
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
